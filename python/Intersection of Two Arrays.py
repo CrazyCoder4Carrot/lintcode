465e74b660b7baa9787b7b5e852e3c7cb3ce3123
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-06-02 17:19:48 -0400

    intersection of two arrays

---
+++ python/Intersection of Two Arrays.py
@@ -0,0 +1,7 @@
+class Solution:
+    # @param {int[]} nums1 an integer array
+    # @param {int[]} nums2 an integer array
+    # @return {int[]} an integer array
+    def intersection(self, nums1, nums2):
+        # Write your code here
+        return list(set(nums1).intersection(set(nums2)))
