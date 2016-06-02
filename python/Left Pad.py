4fc0cb48b84f088f2d8fff528274b709692019ba
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-06-02 18:19:04 -0400

    Left Pad

---
+++ python/Left Pad.py
@@ -0,0 +1,10 @@
+class StringUtils:
+    # @param {string} originalStr the string we want to append to
+    # @param {int} size the target length of the string
+    # @param {string} padChar the character to pad to the left side of the string
+    # @return {string} a string
+    @classmethod
+    def leftPad(self, originalStr, size, padChar=' '):
+        # Write your code here
+        return padChar * (size - len(originalStr)) + originalStr
+        
