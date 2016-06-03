af6b4fe520aa3ca39fbe3fa56ca65674676f80f3
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-06-03 14:30:12 -0400

    merge two sorted list modified

--- python/Merge Two Sorted Lists.py
+++ python/Merge Two Sorted Lists.py
@@ -79,3 +79,32 @@ class Solution:
             cur.next = ListNode(temp)
             cur = cur.next
         return head
+
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+        Recursive
+        3604ms
+"""
+class Solution:
+    """
+    @param two ListNodes
+    @return a ListNode
+    """
+    def mergeTwoLists(self, l1, l2):
+        # write your code here
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            head = l1
+            head.next = self.mergeTwoLists(l1.next, l2)
+        else:
+            head = l2
+            head.next = self.mergeTwoLists(l1, l2.next)
+        return head
