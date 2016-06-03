3195e08e75fb8cc613b945c5f03187ec4a0fd1dd
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-06-03 15:25:53 -0400

    Remove Duplicates from Sorted List

---
+++ python/Remove Duplicates from Sorted List.py
@@ -0,0 +1,21 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    """
+    @param head: A ListNode
+    @return: A ListNode
+    """
+    def deleteDuplicates(self, head):
+        # write your code here
+        cur = head
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
