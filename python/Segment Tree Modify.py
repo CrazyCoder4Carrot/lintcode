711db62be1d328e793ec21f0a0ab150e5e46211e
Zhenbang Liu <liuzhenbang1988@gmail.com>
2016-06-05 23:40:25 -0400

    Segment Tree Modify

---
+++ python/Segment Tree Modify.py
@@ -0,0 +1,30 @@
+"""
+Definition of SegmentTreeNode:
+class SegmentTreeNode:
+    def __init__(self, start, end, max):
+        self.start, self.end, self.max = start, end, max
+        self.left, self.right = None, None
+"""
+
+class Solution:	
+    """
+    @param root, index, value: The root of segment tree and 
+    @ change the node's value with [index, index] to the new given value
+    @return: nothing
+    """
+    def modify(self, root, index, value):
+        # write your code here
+        if not root:
+            return
+        if index < 0:
+            return
+        mid = (root.start + root.end) / 2
+        
+        if index <= mid:
+            self.modify(root.left, index, value)
+        else:
+            self.modify(root.right, index, value)
+        if root.start == root.end:
+            root.max = value
+        else:
+            root.max = max(root.left.max, root.right.max)
