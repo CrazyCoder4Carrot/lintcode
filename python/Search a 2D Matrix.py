7afe2b4b6ee5324f794bdc99bbeedc9144dc1b13
Zhenbang Liu <liuzhenbang1988@gmail.com>
2016-06-05 21:39:53 -0400

    Search a 2D Matrix

---
+++ python/Search a 2D Matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    """
+    @param matrix, a list of lists of integers
+    @param target, an integer
+    @return a boolean, indicate whether matrix contains target
+    """
+    def searchMatrix(self, matrix, target):
+        # write your code here
+        for row in matrix:
+            if target in row:
+                return True
+        return False
