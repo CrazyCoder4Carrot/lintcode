45e4e0fca9553f8d9775ffa4493aec82f7155203
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-06-22 16:18:13 -0400

    merge sort

--- python/Sort Integers II .py
+++ python/Sort Integers II .py
@@ -19,3 +19,42 @@ class Solution:
                 A[i], A[j] = A[j], A[i]
         A[i+1], A[hi] = A[hi], A[i+1]
         return i + 1
+
+
+class Solution:
+    # @param {int[]} A an integer array
+    # @return nothing
+    def sortIntegers2(self, A):
+        # Write your code here
+        b = self.merge_sort(A)
+        for i in range(len(b)):
+            A[i] = b[i]
+    def merge_sort(self, a):
+        if len(a) <= 1:
+            return a
+        mid = (0 + len(a))/2
+        l1 = self.merge_sort(a[:mid])
+        l2 = self.merge_sort(a[mid:])
+        l3 = self.merge(l1, l2)
+        return l3
+    def merge(self, a, b):
+        if not a :
+            return b
+        if not b:
+            return a
+        i, j = 0, 0
+        c = []
+        while i < len(a) and j < len(b):
+            if a[i] < b[j]:
+                c.append(a[i])
+                i += 1
+            else:
+                c.append(b[j])
+                j += 1
+        while i < len(a):
+            c.append(a[i])
+            i += 1
+        while j < len(b):
+            c.append(b[j])
+            j += 1
+        return c
