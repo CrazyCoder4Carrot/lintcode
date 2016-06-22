bd947f3a615af106172bc946d14d96dc1a334ee9
Zhenbang Liu <liuzhenbang1988@gmail.com>
2016-06-22 00:27:54 -0400

    Sort Integers

--- python/Sort Integers.py
+++ python/Sort Integers.py
@@ -3,4 +3,10 @@ class Solution:
     # @return nothing
     def sortIntegers(self, A):
         # Write your code here
-        A.sort()
+        for i in range(1, len(A)):
+            j = i - 1
+            key = A[i]
+            while j >= 0 and A[j] > key:
+                A[j+1] = A[j]
+                j -= 1
+            A[j + 1] = key
